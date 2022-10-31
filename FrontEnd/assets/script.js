3bdacac4549b4bb7f7802a922d95f648881045ca
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2022-10-31 22:38:59 +0100

    Réalisation du filtre des travaux

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,126 +1,113 @@
+const btn_tous = document.getElementById("btn_tous");
+const btn_objets = document.getElementById("btn_objets");
+const btn_appartements = document.getElementById("btn_appartements");
+const btn_hôtels = document.getElementById("btn_hôtels");
+const gallery =  document.querySelector(".gallery");
 
- 
-const btn_tous = document.getElementById('btn_tous');
-const btn_objets = document.getElementById('btn_objets');
-const btn_appartements = document.getElementById('btn_appartements');
-const btn_hôtels = document.getElementById('btn_hôtels');
+fetch("http://localhost:5678/api/works").then((res) => {
+  console.log(res);
+  if (res.ok) {
+    res.json().then((data) => {
+      const numSlid = data.length;
+
+      console.log(data);
 
 
-  fetch ( 'http://localhost:5678/api/works')
-     .then(res => {
-      console.log(res);
-      if (res.ok){
-      res.json( ) .then (data => 
-      {
-
-        
-        const numSlid= data.length;
-      
-       console.log(data)
-       /*
-const img= data[i].imageUrl;
-const title=data[i].title;
-const category = data[i].category;
-const id= data[i].id;
-const categoryId= data[i].categoryId;
-const userId= data[i].userId;
-*/
-function tout(){
-    
-let i = 0
-
-      for (let i = 0; i<=numSlid;i++){
+      //tout
+      function tout() {
        
-        
-        const card  = `
+        document.querySelector(".gallery").innerHTML=""
+       
+
+        for (let i = 0; i <= numSlid; i++) {
+          const card = `
         <figure >
         <img   src="${data[i].imageUrl} ">
        
         <figcaption > ${data[i].title}</figcaption>
         </figure>
-               `    
-     
-      //console.log(data[i].imageUrl)
-      document.querySelector('.gallery') .insertAdjacentHTML( "beforeend",card );
-      console.log(data[i].category.name);
-    }}
-    btn_tous.addEventListener("click",tout);
- 
-   //object
-
- function object(){
-      let i = 0
-      
-       
-            for (let i = 0; i<=numSlid;i++){
+               `;
              
-              if (data[i].category.name == 'Objets'){
-              const card  = `
-              <figure id = ${(data[i].category.Objets)}>
+          //console.log(data[i].imageUrl)
+          document
+            .querySelector(".gallery")
+            .insertAdjacentHTML("beforeend", card);
+          console.log(data[i].category.name);
+        }
+      }
+      btn_tous.addEventListener("click", tout);
+
+      //object
+
+      function object() {
+        document.querySelector(".gallery").innerHTML=""
+
+        for (let i = 0; i <= numSlid; i++) {
+          if (data[i].category.name == "Objets") {
+            const card = `
+              <figure id = ${data[i].category.Objets}>
               <img   src="${data[i].imageUrl} ">
              
               <figcaption > ${data[i].title}</figcaption>
               </figure>
-                     `    
-            document.querySelector('.gallery') .insertAdjacentHTML( "beforeend",card );
+                     `;
+            document
+              .querySelector(".gallery")
+              .insertAdjacentHTML("beforeend", card);
             //console.log(data[i].imageUrl)
-      
           }
-          }}
-  btn_objets.addEventListener("click",object);
+        }
+      }
+      btn_objets.addEventListener("click", object);
 
-//appartements
+      //appartements
 
- function appartements(){
-            let i = 0
-            
-             
-                  for (let i = 0; i<=numSlid;i++){
-                   
-                    if (data[i].category.name == 'Appartements'){
-                    const card  = `
-                    <figure id = ${(data[i].category.Objets)}>
+      function appartements() {
+         
+        document.querySelector(".gallery").innerHTML=""
+        for (let i = 0; i <= numSlid; i++) {
+          if (data[i].category.name == "Appartements") {
+            const card = `
+                    <figure id = ${data[i].category.Objets}>
                     <img   src="${data[i].imageUrl} ">
                    
                     <figcaption > ${data[i].title}</figcaption>
                     </figure>
-                           `    
-                  document.querySelector('.gallery') .insertAdjacentHTML( "beforeend",card );
-                  //console.log(data[i].imageUrl)
-            
-                }
-                }}
-                btn_appartements.addEventListener("click",appartements);
+                           `;
+            document
+              .querySelector(".gallery")
+              .insertAdjacentHTML("beforeend", card);
+            //console.log(data[i].imageUrl)
+          }
+        }
+      }
+      btn_appartements.addEventListener("click", appartements);
 
-
-//hôtels
-function hôtels(){
-  let i = 0
-  
-   
-        for (let i = 0; i<=numSlid;i++){
-         
-          if (data[i].category.name == 'Hotels & restaurants'){
-          const card  = `
-          <figure id = ${(data[i].category.Objets)}>
+      //hôtels
+      function hôtels() {
+ 
+        document.querySelector(".gallery").innerHTML=""
+ 
+        for (let i = 0; i <= numSlid; i++) {
+          if (data[i].category.name == "Hotels & restaurants") {
+            const card = `
+          <figure id = ${data[i].category.Objets}>
           <img   src="${data[i].imageUrl} ">
          
           <figcaption > ${data[i].title}</figcaption>
           </figure>
-                 `    
-        document.querySelector('.gallery') .insertAdjacentHTML( "beforeend",card );
-        //console.log(data[i].imageUrl)
-  
+                 `;
+            document
+              .querySelector(".gallery")
+              .insertAdjacentHTML("beforeend", card);
+            //console.log(data[i].imageUrl)
+          }
+        }
       }
-      }}
-btn_hôtels.addEventListener("click",hôtels);
-
-          
-})
-
-}else {
-console.log("Error");
-document.getElementById('error').innerHTML="Error :("
-}
- 
-})
+      btn_hôtels.addEventListener("click", hôtels);
+    });
+  } else {
+    console.log("Error");
+    document.getElementById("error").innerHTML = "Error :(";
+  }
+});
