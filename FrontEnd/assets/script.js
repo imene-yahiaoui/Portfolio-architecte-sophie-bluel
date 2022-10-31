78c15123a9a7b855ff7b810bab26d2ca2e59ee71
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2022-10-31 20:22:17 +0100

    ajout des btn de filtre

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,42 +1,79 @@
- 
-const image = document.getElementsByClassName('image');
-const titre = document.getElementsByClassName('titre');
-const figure = document.getElementsByClassName('figure');
 
-let count= 0  ;
+ 
+const btn_tous = document.getElementById('btn_tous');
+const btn_objets = document.getElementById('btn_objets');
+const btn_appartements = document.getElementById('btn_appartements');
+const btn_hôtels = document.getElementById('btn_hôtels');
+
 
   fetch ( 'http://localhost:5678/api/works')
-     .then(res => res.json( ))
-     .then (data => 
+     .then(res => {
+      console.log(res);
+      if (res.ok){
+      res.json( ) .then (data => 
       {
+
+        
         const numSlid= data.length;
-        console.log(data)
-const img= data[count].imageUrl;
-const title=data[count].title;
-const category = data[count].category;
-const id= data[count].id;
-const categoryId= data[count].categoryId;
-const userId= data[count].userId;
-
-
-
- 
-
-    
-const card= `
-<figure >
-<img  src="${data[count].imageUrl}" >
-<figcaption > ${data[count].title}</figcaption>
-</figure>
- 
-      `
-   
       
-document.querySelector('.gallery').innerHTML=card;
+       console.log(data)
+       /*
+const img= data[i].imageUrl;
+const title=data[i].title;
+const category = data[i].category;
+const id= data[i].id;
+const categoryId= data[i].categoryId;
+const userId= data[i].userId;
+*/
+function tout(){
+    
+let i = 0
+
+      for (let i = 0; i<=numSlid;i++){
+       
+        
+        const card  = `
+        <figure id ="${data[i].category.name}"   >
+        <img   src="${data[i].imageUrl} ">
+       
+        <figcaption > ${data[i].title}</figcaption>
+        </figure>
+               `    
+     
+      //console.log(data[i].imageUrl)
+
+     
+    }}
+    btn_tous.addEventListener("click",tout);
  
+   
 
-
+ function object(){
+  document.querySelector('.gallery').removeAdjacentHTML("beforeend",card );
+      let i = 0
+      if (data[i].category.name ==data[i].category.Objets){
+            for (let i = 0; i<=numSlid;i++){
+             
+              
+              const card  = `
+              <figure id ="${data[i].category.name}"   >
+              <img   src="${data[i].imageUrl} ">
+             
+              <figcaption > ${data[i].title}</figcaption>
+              </figure>
+                     `    
+            document.querySelector('.gallery') .insertAdjacentHTML( "beforeend",card );
+            //console.log(data[i].imageUrl)
+      
+           
+          }}}
+          btn_objets.addEventListener("click",object);
 
 })
+
+}else {
+console.log("Error");
+document.getElementById('error').innerHTML="Error :("
+}
  
-  
+})
