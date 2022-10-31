0a13c6bad2f3a5e4dbbd2ba036318777036a1bd1
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2022-10-31 01:33:42 +0100

    Récupération des travaux depuis le back-end

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,42 @@
-const url= 'http://localhost:5678/api-docs/';
-fetch (url).then(response =>{return response.json( )}).then (data =>{
-console.log(data);
+ 
+const image = document.getElementsByClassName('image');
+const titre = document.getElementsByClassName('titre');
+const figure = document.getElementsByClassName('figure');
+
+let count= 0  ;
+
+  fetch ( 'http://localhost:5678/api/works')
+     .then(res => res.json( ))
+     .then (data => 
+      {
+        const numSlid= data.length;
+        console.log(data)
+const img= data[count].imageUrl;
+const title=data[count].title;
+const category = data[count].category;
+const id= data[count].id;
+const categoryId= data[count].categoryId;
+const userId= data[count].userId;
+
+
+
+ 
+
+    
+const card= `
+<figure >
+<img  src="${data[count].imageUrl}" >
+<figcaption > ${data[count].title}</figcaption>
+</figure>
+ 
+      `
+   
+      
+document.querySelector('.gallery').innerHTML=card;
+ 
+
+
 
 })
+ 
+  
