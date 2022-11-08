f3c0cfe392ee202cc8274a76e45e740c467a2c0e
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2022-11-08 21:29:52 +0100

    ajoute les photos

--- FrontEnd/assets/javaScript/logIn.js
+++ FrontEnd/assets/javaScript/logIn.js
@@ -1,52 +1,39 @@
-
- 
-const email= document.getElementById("email");
-const password=document.getElementById("password");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 const error = document.getElementById("error");
-const valid = document.getElementById ("login-form-submit");
+const valid = document.getElementById("login-form-submit");
 const form = document.getElementById("login-form");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
- 
-form.addEventListener('submit',function(e){
+  //recupere les inputs
+  const information = new FormData(form);
+  const payload = new URLSearchParams(information);
 
-e.preventDefault();
+  console.log([...payload]);
 
-//recupere les inputs
-const information =new FormData(form);
-const payload= new URLSearchParams(information);
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
 
-console.log([...payload]);
+    body: payload,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      //entre dans la page model
+      if (data.userId == 1) {
+        //recupre le token dans le localStorage
+        localStorage.setItem("token", data.token);
+        //lien ver la page model
+        location.href = "/FrontEnd/index.html";
+      }
 
+      //affiche error
+      else {
+        error.innerText = " Email ou mot de passe ne sont pas correctes";
+      }
+    })
 
- fetch('http://localhost:5678/api/users/login',{
-method:"POST",
- 
-body:payload,
-
-
-})
-.then(res=> res.json())
-.then(data=> {   console.log(data) 
- //entre dans la page model 
-    if( data.userId== 1  ){
-//recupre le token dans le localStorage
-    localStorage.setItem("token",data.token);
-    //lien ver la page model 
-        location.href ="/FrontEnd/index.html";
-    }
-
-        //affiche error 
-else{
-    error.innerText=" Email ou mot de passe ne sont pas correctes";
-}
-})
-
-
-.catch(err=> console.log((err)));
-
-
-}) 
-
-
-
+    .catch((err) => console.log(err));
+});
--- FrontEnd/assets/javaScript/script.js
+++ FrontEnd/assets/javaScript/script.js
@@ -5,8 +5,8 @@ const gallery = document.querySelector(".gallery");
 function info(work) {
   const card = `
     <figure >
-    <img src="${work.imageUrl}">
-      <figcaption>${work.title}</figcaption>
+    <img src="${work?.imageUrl} "crossOrigin="anonymous">
+      <figcaption>${work?.title}</figcaption>
     </figure>
           `;
 
@@ -14,73 +14,85 @@ function info(work) {
 }
 
 //fetch works
-fetch("http://localhost:5678/api/works").then((res) => {
-  console.log(res);
-  if (res.ok) {
-    res.json().then((data) => {
-      const numSlid = data.length;
+fetch("http://localhost:5678/api/works")
+  .then((res) => {
+    console.log(res);
+    if (res.ok) {
+      
+      res.json().then((data) => {
+        
+        console.log(data)
+        const numSlid = data.length;
 
-      //fetch categoris
-      fetch("http://localhost:5678/api/categories").then((res) => {
-        console.log(res);
-        if (res.ok) {
-          res.json().then((category) => {
-            //tout
-            function tout() {
-              document.querySelector(".gallery").innerHTML = "";
+        //fetch categoris
+        fetch("http://localhost:5678/api/categories")
+          .then((res) => {
+            console.log(res);
+            if (res.ok) {
+              res.json().then((category) => {
+                //tout
+                function tout() {
+                  document.querySelector(".gallery").innerHTML = "";
 
-              let i = 0;
-              for (i = 0; i <= numSlid; i++) {
-                info(data[i]);
-              }
-            }
-
-            btn_tous.addEventListener("click", tout);
-
-            /////cree des btn  object////
-            let count = 0;
-
-            console.log();
-
-            for (let count = 0; count <= category.length - 1; count++) {
-              const object = document.createElement("button");
-              object.type = "button";
-              object.innerHTML = category[count].name;
-              object.className = "btn_option";
-              object.onclick = function () {
-                document.querySelector(".gallery").innerHTML = "";
-
-                for (let i = 0; i <= numSlid; i++) {
-                  if (data[i].category.name === category[count].name) {
-                    console.log(count);
+                  let i = 0;
+                  for (i = 0; i <= numSlid-1; i++) {
                     info(data[i]);
                   }
                 }
-              };
-              const button = document.getElementById("btn");
-              button.appendChild(object);
+
+                btn_tous.addEventListener("click", tout);
+
+                /////cree des btn  object////
+                let count = 0;
+
+               
+
+                for (let count = 0; count <= category.length - 1; count++) {
+                  const object = document.createElement("button");
+                  object.type = "button";
+                  object.innerHTML = category[count].name;
+                  object.className = "btn_option";
+                  object.onclick = function () {
+                    document.querySelector(".gallery").innerHTML = "";
+
+                    for (let i = 0; i <= numSlid; i++) {
+                      if (data[i]?.category.name === category[count].name) {
+                        
+                        info(data[i]);
+                      }
+                    }
+                  };
+                  ////cacher les btn dans le mode de login 
+                  if (localStorage.getItem("token")) {console.log("Bienvenu Sophie")}
+                  else { const button = document.getElementById("btn");
+                  button.appendChild(object);}
+                  
+                   
+                }
+               
+               
+                tout();
+              });
+            } else {
+              console.log("Error");
             }
-
-            tout();
-          });
-        } else {
-          console.log("Error");
-        }
+          })
+          .catch((e) => console.log(e));
       });
-    }).catch(err=> console.log((err)));
-  }
-});
+    }
+  })
 
+  .catch((err) => console.log(err));
 
-//entre a la page model 
+//entre a la page model
 if (localStorage.getItem("token")) {
   document.getElementById("login").innerText = "logout";
+document.getElementById("btn").remove(btn_tous);
 
 
 
 }
-//ortier de la page model 
-document.getElementById("login").addEventListener("click", function(){
-
+//sortir de la page model
+document.getElementById("login").addEventListener("click", function () {
   localStorage.clear();
-})
+});
