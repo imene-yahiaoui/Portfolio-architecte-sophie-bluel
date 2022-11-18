b2d07992e2c2e148fda8ee114747f72b4ff65b4a
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2022-11-18 20:25:29 +0100

    organise le code de JavaScript

---
+++ FrontEnd/assets/javaScript/utilisateur.js
@@ -0,0 +1,397 @@
+/////entre a la page model
+if (localStorage.getItem("token")) {
+    //remplacer le login par logout
+    document.getElementById("login").innerText = "logout";
+    //remove btn tout
+    document.getElementById("btn").remove(btn_tous);
+  
+    /////creé div de modification
+  
+    document.getElementById("modifer").style.backgroundColor = "black";
+  
+    //edition
+    const edition = document.createElement("p");
+    edition.type = "button";
+  
+    //////la fonction de modifier/////
+    const modification = `
+  <div>
+  <i class="fa-regular fa-pen-to-square"></i>
+  <p>Mode édition</p>  </div>`;
+    edition.insertAdjacentHTML("afterbegin", modification);
+    edition.className = "edition";
+    edition.onclick = function () {
+     
+      const modifier = `
+      
+  <div id= "modifier">
+  <i class="fa-regular fa-pen-to-square"></i>
+  <p>modifier</p>  </div>`;
+  
+  
+      const modifier_model = `
+  <a href ="#modal"></a>
+  <div id= "modifier_model">
+  <i class="fa-regular fa-pen-to-square"></i>
+  <p>modifier</p>  </div>`;
+  
+      document
+        .getElementById("portfolio_titre")
+        .insertAdjacentHTML("afterend", modifier_model);
+      document
+        .getElementById("introduction_article")
+        .insertAdjacentHTML("afterbegin", modifier);
+      document
+        .getElementById("introduction_photo")
+        .insertAdjacentHTML("beforeend", modifier);
+  
+      ///afficher le photos
+      function photos(works) {
+        const photo_modal = `
+            <figure >
+            
+            <div id="repertoire_modal" class="photo_model_efface">
+            <img src="${works?.imageUrl} "crossOrigin="anonymous">
+           
+            <i id ="${works.id}" class="fa-regular fa-trash-can "></i>
+         
+            </div>
+            
+              <figcaption>éditer</figcaption>
+            </figure>
+                  `;
+  
+        document
+          .getElementById("model_gallery")
+          .insertAdjacentHTML("beforeend", photo_modal);
+      }
+  
+      let token = localStorage.getItem("token");
+      fetch("http://localhost:5678/api/works")
+        .then((res) => {
+          console.log(res);
+          if (res.ok) {
+            res.json().then((data) => {
+              console.log(data);
+              function affiche_model() {
+                let counter = 0;
+                for (counter = 0; counter <= data.length - 1; counter++) {
+                  photos(data[counter]);
+                }
+              }
+  
+              affiche_model();
+              ////////////delet//////////////:
+              for (let counter = 1; counter <= data.length; counter++) {
+                function delet() {
+                  data[counter].id;
+  
+                  console.log(`${data[counter]?.id}`);
+  
+                  fetch("http://localhost:5678/api/works/" + data[counter].id, {
+                    method: "DELETE",
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    },
+                  })
+                    .then((res) => res.json())
+  
+                    .catch((err) => console.log(err));
+                }
+  
+                var id = document.getElementById(`${data[counter]?.id}`);
+                if (id) {
+                  id.addEventListener("click", delet);
+                }
+              }
+            });
+          }
+        })
+  
+        .catch((err) => console.log(err));
+  
+      let page = null;
+  
+      ///////////////////////ouvre modal////////////////////
+      function ouvre_modal(e) {
+        e.preventDefault;
+        const target = document.getElementById("modal");
+        target.style.display = null;
+        target.removeAttribute("aria-hidden");
+        page = target;
+        page?.addEventListener("click", ferme_modal);
+        page
+          .querySelector(".js_modal_stop")
+          .addEventListener("click", stopPropagation);
+        //le modal ferme quand on click d'hors
+      }
+  
+      document
+        .getElementById("modifier_model")
+        .addEventListener("click", ouvre_modal);
+      //////la fleche de retoure
+      document.getElementById("left").addEventListener("click", ouvre_modal);
+  
+      /////ouvre modal avec clavier
+      window.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+          ouvre_modal(e);
+        }
+      });
+  
+      //pour stop  la fermeteur le model quand en click dessu
+  
+      const stopPropagation = function (e) {
+        e.stopPropagation();
+      };
+  
+      ///////////:////FERMER le modal//////////////
+      function ferme_modal(e) {
+        e.preventDefault;
+        const page = document.getElementById("modal");
+        page.style.display = "none";
+        page.setAttribute("aria-hidden", "true");
+        page?.removeEventListener("click", ferme_modal);
+      }
+      document
+        .getElementById("model_fermer")
+        .addEventListener("click", ferme_modal);
+      document
+        .getElementById("model_ajoute")
+        .addEventListener("click", ferme_modal);
+  
+      /////////////ferme modal avec clavier
+  
+      window.addEventListener("keydown", function (e) {
+        if (e.key === "Escape" || e.key === "Esc") {
+          ferme_modal(e);
+        }
+      });
+      ///////desactiver btn modifier ///////////
+      edition.onclick = function () {
+        document
+          .getElementById("portfolio_titre")
+          .removeAttribute("modifier_model");
+        document
+          .getElementById("introduction_article")
+          .removeAttribute("modifier");
+        document.getElementById("introduction_photo").removeAttribute("modifier");
+        document
+        .getElementById("modifier").style.display=null;
+        document .getElementById("modifier_model").style.display=null;
+      };
+    };
+  
+    const edit = document.getElementById("modifer");
+    edit.appendChild(edition);
+  
+    /////ouvre la page ajoute photo///
+  
+    let model_ajout = null;
+    //ouvre modal
+    function ouvre_modal_ajoute(e) {
+      e.preventDefault;
+      model_page = document.getElementById("modal_ajout");
+      model_page.style.display = null;
+      model_page.removeAttribute("aria-hidden");
+      model_ajout = model_page;
+      model_ajout?.addEventListener("click", ferme_modal_ajoute);
+      model_ajout
+        .querySelector(".js_modal_stop")
+        .addEventListener("click", stopPropagation);
+      //le modal ferme quand on click d'hors
+    }
+  
+    document
+      .getElementById("model_ajoute")
+      .addEventListener("click", ouvre_modal_ajoute);
+  
+    //ouvre modal avec clavier
+    window.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {
+        ouvre_modal_ajoute(e);
+      }
+    });
+  
+    //pour stoper fermer le model quand en click dessu
+  
+    const stopPropagation = function (e) {
+      e.stopPropagation();
+    };
+  
+    /////telecharger les photos/////
+    function telecharger() {
+      const input = document.getElementById("img_input");
+  
+      var telecharger_image = "";
+  
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        telecharger_image = reader.result;
+        const photo = document.getElementById("image_telecharger");
+        document.getElementById("image_telecharger_images").style.display = null;
+  
+        photo.style.backgroundImage = `url(${telecharger_image} )`;
+        document.getElementById("model_ajout_container").style.display = "none";
+      });
+      reader.readAsDataURL(this.files[0]);
+    }
+  
+    document.getElementById("img_input").addEventListener("change", telecharger);
+  
+    ////Envoi de fichiers via un objet FormData
+    document
+      .getElementById("modal_ajout")
+      .addEventListener("submit", function (e) {
+        e.preventDefault();
+  
+        const photo = document.getElementById("img_input");
+        const category = document.getElementById("categorie");
+        const title = document.getElementById("input_model");
+        
+  
+        // un msg err si le formulair pas rempli
+        if (
+          photo.value === null ||
+          photo.value === "" ||
+          category.value === null ||
+          title.value === null ||
+          title.value === "" ||
+          category.value === ""
+        ) {
+          document.getElementById("msg_err").innerHTML =
+            "il faut remplire le formulair ";
+        } else {
+          document.getElementById("msg_err").innerHTML = "";
+  
+          fetch("http://localhost:5678/api/categories").then((res) => {
+            console.log(res);
+            if (res.ok) {
+              res.json().then((categorydata) => {
+                for (let i = 0; i <= categorydata.length - 1; i++) {
+                  if (category.value === categorydata[i].name) {
+                    categorydata[i].name = categorydata[i].id;
+                    console.log(categorydata[i].id);
+                    console.log(category.value);
+  
+                    const image = document.getElementById("img_input").files[0];
+  
+                    let token = localStorage.getItem("token");
+                    console.log(`Bearer  ${token}`);
+                    const titre = document.getElementById("input_model").value;
+  
+                    //////le size de la photo////
+                    if (image.size < 4 * 1048576) {
+                      const formData = new FormData();
+                      formData.append("image", image);
+                      formData.append("title", titre);
+                      formData.append("category", categorydata[i].id);
+  
+                      //fetch works
+  
+                      fetch("http://localhost:5678/api/works", {
+                        method: "POST",
+                        headers: {
+                          Authorization: `Bearer ${token}`,
+                        },
+                        body: formData,
+                      })
+                        .then((res) => {
+                          console.log(res);
+                          if (res.ok) {
+                            res.json().then((data) => {
+                              console.log(data);
+                              
+                          alert(`le projet ${data.title} est ajouter`)
+                        
+                            });
+                          }
+                        })
+                        .then((res) => res.json())
+  
+                        
+                        .catch((err) => console.log("il ya un problem" + err));
+                    } else {
+                      console.log("la photo est trop grand ");
+                    }
+  
+                    const input_photo_url = document.getElementById("img_input");
+                    input_photo_url.value = null;
+  
+                    const titre_sansvalu = document.getElementById("input_model");
+                    titre_sansvalu.value = null;
+                    const category_sans_valu =
+                      document.getElementById("categorie");
+                    category_sans_valu.value = null;
+                    // suprim les doner quand en ferme
+                    document.getElementById(
+                      "model_ajout_container"
+                    ).style.display = null;
+                    document.getElementById(
+                      "image_telecharger_images"
+                    ).style.display = "none";
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
+  
+    ////FERMER le modal de ajout photo ///
+    function ferme_modal_ajoute(e) {
+      e.preventDefault;
+      const model_ajout = document.getElementById("modal_ajout");
+      model_ajout.style.display = "none";
+      model_ajout.setAttribute("aria-hidden", "true");
+      model_ajout?.removeEventListener("click", ferme_modal_ajoute);
+  
+      // suprim les doner quand en ferme
+      document.getElementById("model_ajout_container").style.display = null;
+      document.getElementById("image_telecharger_images").style.display = "none";
+      //suprime les doner de titre
+  
+      const input_titre_ajout = document.getElementById("input_model");
+      input_titre_ajout.value = null;
+  
+      //suprime le url des photos
+      const input_photo_url = document.getElementById("img_input");
+      input_photo_url.value = null;
+  
+      //suprime les donner de categore
+      const category = document.getElementById("categorie");
+      category.value = category.value[0];
+  
+      //suprimer msg err
+      document.getElementById("msg_err").innerHTML = "";
+    }
+    document
+      .getElementById("model_fermer_ajouter")
+      .addEventListener("click", ferme_modal_ajoute);
+    //la fleche de retoure
+    document.getElementById("left").addEventListener("click", ferme_modal_ajoute);
+  
+    //ferme modal avec clavier
+  
+    window.addEventListener("keydown", function (e) {
+      if (e.key === "Escape" || e.key === "Esc") {
+        ferme_modal_ajoute(e);
+      }
+    });
+  
+    ///////////////publier les changements
+    const changment = document.createElement("button");
+    changment.type = "button";
+  
+    const modification_changment = `
+  <p>publier les changements</p>  `;
+    changment.insertAdjacentHTML("beforeend", modification_changment);
+    changment.className = "publier";
+    changment.onclick = function () {
+        location.href = "/FrontEnd/index.html";
+    
+  
+    };
+    const changements = document.getElementById("modifer");
+    changements.appendChild(changment);
+  }
